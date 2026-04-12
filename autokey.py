71061b0969328b02af02ea3b4090d22136ff83c9
иван <onebonik@gmail.com>
2026-04-12 13:02:47 +0400

    можно играть на телефоне

--- autokey.py
+++ autokey.py
@@ -4,13 +4,12 @@ import time
 time.sleep(5)  # 5 секунд чтобы ты успел переключиться в консоль
 
 text = """
-sudo certbot certonly --standalone -d friend-games.ru -d www.friend-games.ru --agree-tos --email onebonik@gmail.com --non-interactive
+sudo docker volume prune
 """
 
 for line in text.split("\n"):
     pyautogui.typewrite(line)
     pyautogui.press("enter")
     time.sleep(0.5)
-
 
 
